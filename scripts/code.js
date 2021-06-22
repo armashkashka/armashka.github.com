a7e6f1a5295151e4377b3394a196005ad4def927
armashka <61916931+armashkashka@users.noreply.github.com>
2021-06-22 12:41:54 +0600

    init

--- scripts/code.js
+++ scripts/code.js
@@ -1,16 +1,1 @@
-var t1=0;function check(){var wd=$(window).width(),hi=$(window).height();if(wd<hi&&!t1){var str=window.location.href;console.log(str);if(str=="")return;while(str.charAt(str.length-1)!='/')str=str.slice(0,-1);str+="mobile.html";window.location.replace(str)console.log(str)}}
-
-/*var t1 = 0;
-
-function check() {
-	var wd = $(window).width(), hi = $(window).height();
-	if (wd < hi && !t1) {
-		var str = window.location.href;
-		console.log(str);
-		if (str == "") return;
-		while (str.charAt(str.length - 1) != '/') str = str.slice(0, -1);
-		str += "mobile.html";
-		window.location.replace(str)
-		console.log(str);
-	}
-}*/
+function check(){var wd=$(window).width(),hi=$(window).height();if(wd<hi){var str=window.location.href;while(str.charAt(str.length-1)!='/')str=str.slice(0,-1);str+="mobile.html";window.location.replace(str)}}
