29e52bd67fccb53de764aa2bd1a1d8ee51f43feb
armashka <61916931+armashkashka@users.noreply.github.com>
2021-06-22 12:34:56 +0600

    init

--- scripts/mobile-code.js
+++ scripts/mobile-code.js
@@ -1,4 +1,6 @@
-function aw() {
+function aw(){$('.menu').css("width","75%")}function jap(){$('.menu').css("width","0%")}var t1=0;function check(){var wd=$(window).width(),hi=$(window).height();if(wd>hi&&!t1){var str=window.location.href;console.log(str);if(str=="")return;while(str.charAt(str.length-1)!='/')str=str.slice(0,-1);str+="index.html";window.location.replace(str)console.log(str)}}
+
+/*function aw() {
 	$('.menu').css("width", "75%");
 }
 
@@ -19,4 +21,4 @@ function check() {
 		window.location.replace(str)
 		console.log(str);
 	}
-}
+}*/
